d2aa2ac944886c28f4b18d829cb82e5ff5359fe3
rahoyoun <raho@yonsei.ac.kr>
2026-05-25 21:22:10 +0900

    ddddd

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html", message="Hello, World!")
+def home():
+    return render_template(
+        "index.html",
+        message="Coinfographic Flask Server Running"
+    )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=9000)
